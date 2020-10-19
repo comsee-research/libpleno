06789ea189c9c6bef8ce82f8c2eb689649045185
Mathieu Labussiere <mathieu.labu@gmail.com>
2020-10-19 16:56:33 +0200

    [src] fix default parameter in camera config

--- pleno/io/cfg/camera.h
+++ pleno/io/cfg/camera.h
@@ -17,9 +17,9 @@ V_DEFINE_PROPERTIES(PlenopticCameraConfig)
     V_DEFINE_PROPERTY(main_lens, ThinLensConfig(), "Main Lens configuration")    
     V_DEFINE_PROPERTY(distortions, DistortionConfig(), "Distortions coefficients (radial and tangential)")
     
-    V_DEFINE_PROPERTY(dist_focus, double(1e8), "Focus distance (in mm)")
+    V_DEFINE_PROPERTY(dist_focus, double(1e8), "Focus distance (in mm). Warn: inf ~ 1e8")
     V_DEFINE_PROPERTY(I, std::size_t(3u), "Number of micro-lenses types (0: act as pinholes array)")
-    V_DEFINE_PROPERTY(mode, (int(-1)), "Mode of the camera (0: Unfocused (F=D and f=d); 1: Keplerian (F<D and f<d)) ; 2: Galilean (F>D and f>d)")
+    V_DEFINE_PROPERTY(mode, (int(2)), "Mode of the camera (0: Unfocused (F=D and f=d); 1: Keplerian (F<D and f<d)) ; 2: Galilean (F>D and f>d)")
 )
 
 using MultiFocusPlenopticCameraConfig = PlenopticCameraConfig;
